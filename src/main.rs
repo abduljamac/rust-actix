9fd52df2aab6e00c1e50a22d509f54f1b11ef00b
abduljamac <abduljama001@gmail.com>
2023-02-04 18:21:06 +0000

    log the port on run

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,7 @@ mod repository;
 use api::task::create_task;
 use repository::db::MongoRepo;
 
-use actix_web::{get, middleware::Logger, web::Data, App, HttpResponse, HttpServer, Responder};
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
+use actix_web::{middleware::Logger, web::Data, App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -19,6 +14,9 @@ async fn main() -> std::io::Result<()> {
     env_logger::init();
     let db = MongoRepo::init().await;
     let db_data = Data::new(db);
+
+    let port = 3000;
+    println!("Starting server at port {}", port);
     HttpServer::new(move || {
         let logger = Logger::default();
         App::new()
@@ -26,7 +24,7 @@ async fn main() -> std::io::Result<()> {
             .app_data(db_data.clone())
             .service(create_task)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", port))?
     .run()
     .await
 }
